fd1db0baa01842b2483dfa66e82b61829d05d415
sidharth-devlpr <sidharthschandran@gmail.com>
2024-03-02 15:01:33 +0530

    #1 commit to ratings data service

---
+++ src/main/java/com/sidharthdevlpr/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sidharthdevlpr.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
